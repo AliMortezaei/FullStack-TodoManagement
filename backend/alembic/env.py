37cf4560291351f0cdbe561ab0bc1c41088dd8e3
AlirezMortezaei <Mortezaei2324@gmail.com>
2024-02-27 13:25:16 +0330

    🚧 Update alembic config file

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -6,7 +6,7 @@ from sqlmodel import SQLModel
 
 from alembic import context
 
-from models import *  
+from db.models import *  
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
